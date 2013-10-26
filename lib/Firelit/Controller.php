06bced351994b58bfc09071fb9f035d7bf30687e
Eric Bigoness <eric.bigoness@firelit.com>
2013-10-26 15:03:43 -0400

    Simplifying controller

--- lib/Firelit/Controller.php
+++ lib/Firelit/Controller.php
@@ -4,77 +4,31 @@ namespace Firelit;
 
 abstract class Controller {
 	
-	static public $request, $response, $router, $authenticator;
-	static public $debug = false;
-	static public $page = 0, $pagesize = 50, $url;
-	
 	abstract public function __construct();
 	
-	static public function setup(Firelit\Request $request, Firelit\Response $response, Router $router = null, Authenticator $authenticator = null) {
+	/**
+	 * Handoff control to a/another controller
+	 *
+	 * @param  string $controller
+	 * @param  string|bool $method
+	 * @return mixed
+	 */
+	static public function handoff($controller, $method = false) {
 	
-		self::$request = $request;
-		self::$response = $response;
-		self::$router = $router;
-		self::$authenticator = $authenticator;
-
-		if (isset( self::$request->get['page'] )) {
-			self::$page = intval( self::$request->get['page'] );
-			if (self::$page < 0) self::$page = 0;
-		}
-
-		if (isset( self::$request->get['pagesize'] )) {
-			self::$pagesize = intval( self::$request->get['pagesize'] );
-			if (self::$pagesize < 10) self::$pagesize = 10;
-			if (self::$pagesize > 250) self::$pagesize = 250;
-		}
+		$args = func_get_args();	
 		
-	}
-	
-	static public function handoff($resource, $action = '') {
+		// If parameters passed, be sure to pass them on:
+		if (sizeof($args)) array_shift($args);
+		if (sizeof($args)) array_shift($args);
 		
-		try {
+		$reflect  = new \ReflectionClass($controller);
+		$return = $reflect->newInstanceArgs($args);
 
-			$controller = $resource . __class__ . $action;
-			
-			$args = func_get_args();	
-			
-			if (sizeof($args) <= 2) 
-				return new $controller();
-			
-			// If parameters passed, be sure to pass them on:
-			array_shift($args);
-			array_shift($args);
-			
-			$reflect  = new \ReflectionClass($controller);
-			return $reflect->newInstanceArgs($args);
-			
-		} catch (ControllerError $e) {
-
-			self::$response->code( $e->getCode() );
-			self::$response->set( array('error' => $e->getMessage()) );
-
-			if (self::$debug) {
-
-				$debugArray = array(
-					'file' => $e->getFile(),
-					'line' => $e->getLine(),
-					'trace' => $e->getTraceAsString()
-				);
-
-				$prev = $e->getPrevious();
-				if (is_object($prev))
-					$debugArray['previous'] = $prev->getMessage();
-
-				self::$response->set( array('debug' => $debugArray) );
-			
-			}
-			
-			self::$response->respond();
-
-			exit;
-
-		}
-
+		// If method specified, return the result of that method call
+		if ($method) return $return->$method;
+		// Else return the object itself
+		else return $return;
+		
 	}
 
 }
