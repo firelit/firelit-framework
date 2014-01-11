307e307877a9d8b2394ee6aad94b72641b2c279d
Eric Bigoness <eric.bigoness@firelit.com>
2014-01-11 15:59:13 -0500

    Added Database Migration classes

---
+++ tests/DatabaseMigrationManagerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+class DatabaseMigrationMockTemplate extends Firelit\DatabaseMigration {
+	public static $upCount = 0, $downCount = 0;
+	public function up() { static::$upCount++; }
+	public function down() { static::$downCount++; }
+}
+
+class DatabaseMigrationMock_1 extends DatabaseMigrationMockTemplate {
+	public static $upCount = 0, $downCount = 0;
+	static public $version = '4.5';
+}
+
+class DatabaseMigrationMock_2 extends DatabaseMigrationMockTemplate {
+	public static $upCount = 0, $downCount = 0;
+	static public $version = '5.0';
+}
+
+class DatabaseMigrationMock_3 extends DatabaseMigrationMockTemplate {
+	public static $upCount = 0, $downCount = 0;
+	static public $version = '5.0.1';
+}
+
+class DatabaseMigrationManagerTest extends PHPUnit_Framework_TestCase {
+
+	public function testSimple() {
+
+		$mock = new DatabaseMigrationMock_1();
+
+		$manager = new Firelit\DatabaseMigrationManager('4.4.12');
+
+		$manager->submitMigration($mock);
+
+		$this->assertEquals(1, $manager->count());
+
+	}
+
+	public function testUpMigration() {
+
+		$mock1 = new DatabaseMigrationMock_1();
+		$mock2 = new DatabaseMigrationMock_2();
+		$mock3 = new DatabaseMigrationMock_3();
+
+		$manager = new Firelit\DatabaseMigrationManager('4.4.12', 'up', '5.0');
+
+		$manager->submitMigration($mock1);
+		$this->assertEquals(1, $manager->count());
+
+		$manager->submitMigration($mock2);
+		$this->assertEquals(2, $manager->count());
+
+		$manager->submitMigration($mock3);
+		$this->assertEquals(2, $manager->count());
+
+	}
+
+	public function testDownMigration() {
+
+		$mock1 = new DatabaseMigrationMock_1();
+		$mock2 = new DatabaseMigrationMock_2();
+
+		$manager = new Firelit\DatabaseMigrationManager('5.0', 'down', '4.5.1');
+
+		$manager->submitMigration($mock1);
+		$manager->submitMigration($mock2);
+
+		$this->assertEquals(1, $manager->count());
+		
+	}
+
+	public function testSortAndCallback() {
+
+		$manager = new Firelit\DatabaseMigrationManager('4.4');
+
+		$manager->submitMigration('DatabaseMigrationMock_2');
+		$manager->submitMigration('DatabaseMigrationMock_3');
+		$manager->submitMigration('DatabaseMigrationMock_1');
+
+		$this->assertEquals(3, $manager->count());
+
+		$manager->sortMigrations();
+
+		$test = $this;
+
+		$manager->setPostExecCallback(function($ver, $on, $of) use ($test) {
+
+			if ($on == 0) {
+				$test->assertEquals(1, DatabaseMigrationMock_1::$upCount);
+				$test->assertEquals(0, DatabaseMigrationMock_2::$upCount);
+				$test->assertEquals(0, DatabaseMigrationMock_3::$upCount);
+			} elseif ($on == 1) {
+				$test->assertEquals(1, DatabaseMigrationMock_1::$upCount);
+				$test->assertEquals(1, DatabaseMigrationMock_2::$upCount);
+				$test->assertEquals(0, DatabaseMigrationMock_3::$upCount);
+			} elseif ($on == 2) {
+				$test->assertEquals(1, DatabaseMigrationMock_1::$upCount);
+				$test->assertEquals(1, DatabaseMigrationMock_2::$upCount);
+				$test->assertEquals(1, DatabaseMigrationMock_3::$upCount);
+			} else {
+				throw new Exception('Invalid loop index: '. $on);
+			}
+			
+		});
+
+		$manager->executeMigrations();
+
+	}
+
+}
---
+++ tests/DatabaseMigrationTest.php
@@ -0,0 +1,73 @@
+<?PHP
+
+class DatabaseMigrationMock extends Firelit\DatabaseMigration {
+
+	static protected $version = '5.0.5';
+
+	public function up() { }
+	public function down() { }	
+
+}
+
+class DatabaseMigrationTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetVersion() {
+
+		$mock = new DatabaseMigrationMock();
+
+		$this->assertEquals('5.0.5', $mock->getVersion());
+
+	}
+
+	public function testCheckVersionUp() {
+
+		$mock = new DatabaseMigrationMock();
+
+		$this->assertTrue($mock->checkVersionUp('4.15.15'));
+		$this->assertTrue($mock->checkVersionUp('5.0'));
+		$this->assertTrue($mock->checkVersionUp('5.0.4'));
+
+		$this->assertFalse($mock->checkVersionUp('5.0.5'));
+
+		$this->assertFalse($mock->checkVersionUp('5.0.6'));
+		$this->assertFalse($mock->checkVersionUp('5.1'));
+		$this->assertFalse($mock->checkVersionUp('6.0'));
+
+	}
+
+	public function testCheckVersionUpLimit() {
+
+		$mock = new DatabaseMigrationMock();
+
+		$this->assertTrue($mock->checkVersionUp('5.0.4', '5.0.5'));
+		$this->assertTrue($mock->checkVersionUp('5.0.3', '5.0.6'));
+		$this->assertFalse($mock->checkVersionUp('5.0.4', '5.0.4'));
+		$this->assertFalse($mock->checkVersionUp('5.0.3', '5.0.4'));
+
+		$this->assertFalse($mock->checkVersionUp('5.0.5', '5.0.2'));
+		$this->assertFalse($mock->checkVersionUp('5.0.5', '5.0.6'));
+
+		$this->assertFalse($mock->checkVersionUp('5.0.6', '5.0.2'));
+		$this->assertFalse($mock->checkVersionUp('5.0.6', '5.0.6'));
+
+	}
+
+	public function testCheckVersionDown() {
+
+		$mock = new DatabaseMigrationMock();
+
+		$this->assertFalse($mock->checkVersionDown('5.0', '4.15.1'), 'Current version is below mock migration version');
+		$this->assertFalse($mock->checkVersionDown('5.0', '5.0.5'), 'Current version is below mock migration version');
+		$this->assertFalse($mock->checkVersionDown('5.0', '5.2'), 'Current version is below mock migration version');
+
+		$this->assertTrue($mock->checkVersionDown('5.0.5', '4.15.1'), 'Current version matches, target is below');
+		$this->assertFalse($mock->checkVersionDown('5.0.5', '5.0.5'), 'Current version matches, but target is same');
+		$this->assertFalse($mock->checkVersionDown('5.0.5', '5.1'), 'Current version matches, but target is above');
+
+		$this->assertTrue($mock->checkVersionDown('5.1', '4.16'), 'Current version ahead of mock and target is below');
+		$this->assertFalse($mock->checkVersionDown('5.1', '5.0.5'), 'Current version ahead of mock but target is same');
+		$this->assertFalse($mock->checkVersionDown('5.1', '5.1.1'), 'Current version ahead of mock but target is above');
+
+	}
+
+}
