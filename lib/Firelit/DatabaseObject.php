536012f4541d93f1c9677fdcbcc75450b28caa28
Eric Bigoness <eric.bigoness@firelit.com>
2014-01-14 19:51:36 -0500

    Added underscore to protected vars to prevent conflict with magic setters/getters

--- lib/Firelit/DatabaseObject.php
+++ lib/Firelit/DatabaseObject.php
@@ -8,9 +8,9 @@ class DatabaseObject {
 	protected static $tableName = 'ReplaceMe'; // The table name
 	protected static $primaryKey = 'id'; // The primary key for table (or false if n/a)
 
-	protected $data = array(); // The object's data
-	protected $dirty = array(); // Array of dirty properties that need saving
-	protected $new = false; // Indicates a new, unsaved object
+	protected $_data = array(); // The object's data
+	protected $_dirty = array(); // Array of dirty properties that need saving
+	protected $_new = false; // Indicates a new, unsaved object
 	
 	protected static $query = false;
 
@@ -18,12 +18,12 @@ class DatabaseObject {
 
 		// If it doesn't come pre-loaded, it's new
 		// (Pre-loading made possible by PDOStatement::fetchObject)
-		if (!sizeof($this->data)) $this->new = true;
+		if (!sizeof($this->_data)) $this->_new = true;
 
-		$this->dirty = array(); // Reset in case pre-loaded
+		$this->_dirty = array(); // Reset in case pre-loaded
 
 		if (is_array($data))
-			$this->data = $data;
+			$this->_data = $data;
 
 	}
 
@@ -34,86 +34,86 @@ class DatabaseObject {
 
 	public function save() {
 
-		if (!$this->new && !sizeof($this->dirty)) return;
+		if (!$this->_new && !sizeof($this->_dirty)) return;
 
 		if (static::$query) $q = static::$query;
 		else $q = new Query();
 		
-		if ($this->new) {
+		if ($this->_new) {
 
-			$q->insert(static::$tableName, $this->data);
+			$q->insert(static::$tableName, $this->_data);
 
 			// If new, set data id from autoincrement id
 			if (static::$primaryKey)
-				$this->data[static::$primaryKey] = $q->getNewId();
+				$this->_data[static::$primaryKey] = $q->getNewId();
 
 		} else {
 
 			if (!static::$primaryKey) 
 				throw new \Exception('Cannot perform update without a primary key.');
 
-			if (!isset($this->data[static::$primaryKey]))
+			if (!isset($this->_data[static::$primaryKey]))
 				throw new \Exception('Cannot perform update without primary key set.');
 
 			$updateData = array(); 
 
-			foreach ($this->dirty as $key) {
-				if (isset($this->data[$key]))
-					$updateData[$key] = $this->data[$key];
+			foreach ($this->_dirty as $key) {
+				if (isset($this->_data[$key]))
+					$updateData[$key] = $this->_data[$key];
 			}
 
 			if (isset($updateData[static::$primaryKey]))
 				throw new \Exception('Cannot perform update on primary key (it was marked dirty).');
 
-			$q->update(static::$tableName, $updateData, "WHERE `". static::$primaryKey ."`='". $this->data[static::$primaryKey] ."' LIMIT 1");
+			$q->update(static::$tableName, $updateData, "WHERE `". static::$primaryKey ."`='". $this->_data[static::$primaryKey] ."' LIMIT 1");
 			
 		}
 		
-		$this->new = false;
-		$this->dirty = array();
+		$this->_new = false;
+		$this->_dirty = array();
 
 	}
 
 	public function setNew() {
-		$this->new = true;
-		$this->dirty = array();
+		$this->_new = true;
+		$this->_dirty = array();
 	}
 
 	public function setNotNew() {
-		$this->new = false;
+		$this->_new = false;
 	}
 
 	public function isNew() {
-		return $this->new;
+		return $this->_new;
 	}
 
 	public function getDirty() {
-		return $this->dirty;
+		return $this->_dirty;
 	}
 
 	public function __get($var) {
 
-		if (isset($this->data[$var])) return $this->data[$var];
+		if (isset($this->_data[$var])) return $this->_data[$var];
 		else return null;
 
 	}
 
 	public function __set($var, $val) {
 
-		$this->data[$var] = $val;
+		$this->_data[$var] = $val;
 
-		if (!$this->new && !in_array($var, $this->dirty))
-			$this->dirty[] = $var;
+		if (!$this->_new && !in_array($var, $this->_dirty))
+			$this->_dirty[] = $var;
 
 	}
 
 	public function __clone() {
 		
 		if (static::$primaryKey)
-			unset($this->data[static::$primaryKey]);
+			unset($this->_data[static::$primaryKey]);
 		
-		$this->new = true;
-		$this->dirty = array();
+		$this->_new = true;
+		$this->_dirty = array();
 
 	}
 
@@ -122,17 +122,17 @@ class DatabaseObject {
 		if (!static::$primaryKey) 
 			throw new \Exception('Cannot perform delete without a primary key.');
 
-		$this->data = array();
-		$this->dirty = array();
+		$this->_data = array();
+		$this->_dirty = array();
 
-		if ($this->new) return;
+		if ($this->_new) return;
 		
 		$sql = "DELETE FROM `". static::$tableName ."` WHERE `". static::$primaryKey ."`=:id LIMIT 1";
 
 		if (static::$query) $q = static::$query;
 		else $q = new Query();
 
-		$q->query($sql, array( ':id' => $this->data[static::$primaryKey] ));
+		$q->query($sql, array( ':id' => $this->_data[static::$primaryKey] ));
 
 	}
 
