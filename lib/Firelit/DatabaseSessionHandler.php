6aeb86d7fa0d466f988e89d4bb30767fa0ebceea
Eric Bigoness <eric.bigoness@firelit.com>
2014-01-23 17:10:10 -0500

    Fixed bug in database session handler read

--- lib/Firelit/DatabaseSessionHandler.php
+++ lib/Firelit/DatabaseSessionHandler.php
@@ -28,7 +28,8 @@ class DatabaseSessionHandler implements \SessionHandlerInterface {
 			':session_id' => $id
 		));
 
-		return $q->getRow();
+		if (!$data) return false;
+		return $data[ self::$config['colData'] ];
 
     }
 
