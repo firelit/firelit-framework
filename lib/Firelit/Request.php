21ea48bed5be33eb9f6c7e3960254501992c2c41
Eric Bigoness <eric.bigoness@firelit.com>
2016-01-14 11:43:08 -0500

    Specify which property is not valid

--- lib/Firelit/Request.php
+++ lib/Firelit/Request.php
@@ -3,15 +3,15 @@
 namespace Firelit;
 
 class Request extends Singleton {
-	
+
 	// All properties accessible via magic getter method
 	private $ip, $proxies, $host, $path, $method, $secure, $referer, $protocol, $cli, $headers, $uri, $body;
 	private $put, $post, $get, $cookie;
-	
+
 	public static $loadBalanced = false;
 
 	// $filter should be a filtering function, if supplied, which filters a string value by reference
-	public function __construct($filter = false, $bodyFormat = 'querystring') { 
+	public function __construct($filter = false, $bodyFormat = 'querystring') {
 
 		$this->ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : false;
 		$this->proxies = array();
@@ -24,38 +24,38 @@ class Request extends Singleton {
 
 		$this->cli = (php_sapi_name() == 'cli');
 		if ($this->cli) $this->method = 'CLI';
-		
+
 		$this->uri = ($this->cli ? false : ($this->secure ? 'https' : 'http') .'://'. $this->host . $this->path);
-		
+
 		if (is_callable('apache_request_headers')) {
-			
+
 			$this->headers = apache_request_headers();
-			
+
 			if (self::$loadBalanced) {
-				
+
 				if (isset($this->headers['X-Forwarded-For'])) {
 					$ips = $this->headers['X-Forwarded-For'];
 					$ips = explode(', ', $ips);
 					$this->ip = array_shift($ips);
-					
+
 					$this->proxies = $ips;
 				}
-				
+
 				if (isset($this->headers['X-Forwarded-Proto'])) {
 					$this->secure = ($this->headers['X-Forwarded-Proto'] == 'HTTPS');
 				}
-				
+
 			} elseif (isset($this->headers['X-Forwarded-For'])) {
-				
+
 				$ips = $this->headers['X-Forwarded-For'];
 				$ips = explode(', ', $ips);
 				$this->proxies = $ips;
-				
+
 			}
-		
+
 		} else
 			$this->headers = array();
-		
+
 		global $_PUT;
 		$_PUT = array();
 
@@ -66,7 +66,7 @@ class Request extends Singleton {
 		}
 
 		if ($bodyFormat == 'json') {
-			
+
 			$this->put = array();
 			$this->post = array();
 
@@ -74,7 +74,7 @@ class Request extends Singleton {
 				$this->put = json_decode($this->body, true);
 			elseif ($this->method == 'POST')
 				$this->post = json_decode($this->body, true);
-			
+
 		} else {
 			$this->post = $_POST;
 			$this->put = $_PUT;
@@ -82,56 +82,56 @@ class Request extends Singleton {
 
 		$this->get = $_GET;
 		$this->cookie = $_COOKIE;
-		
+
 		if ($filter) {
 			// Filter local copies of POST, GET & COOKIE data
 			// Unset global versions to prevent access to un-filtered
 			$this->filterInputs($filter);
-			
+
 			$_PUT = null;
 			$_POST = null;
 			$_GET = null;
 			$_COOKIE = null;
-			
+
 		}
 
 	}
-	
+
 	public function filterInputs($filter = false) {
-		
+
 		if ($filter == false) return;
-		if (!is_callable($filter)) 
+		if (!is_callable($filter))
 			throw new \Exception('Specified filter is not callable.');
-		
+
 		$this->recurse($this->post, $filter);
 		$this->recurse($this->put, $filter);
 		$this->recurse($this->get, $filter);
 		$this->recurse($this->cookie, $filter);
-	
+
 	}
-	
+
 	protected function recurse(&$input, &$function) {
-	
+
 		if (is_array($input))
 			foreach ($input as $name => &$value)
 				$this->recurse($value, $function);
 		else
 			$function($input);
-			
+
 	}
-	
+
 	public function __get($name) {
-	
+
 		if (isset($this->$name)) return $this->$name;
-		
-		throw new \Exception('Invalid property specified.');
-		
+
+		throw new \Exception('The property "'. $name .'" is not valid.');
+
 	}
 
 	public function __isset($name) {
-	
+
 		return isset($this->$name);
-		
+
 	}
 
 }
